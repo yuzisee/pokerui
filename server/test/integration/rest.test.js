8eaffca7d8db22d5cc33d90976d7110b77bf445a
Andrew Jo <andrewjo@gmail.com>
2014-02-22 19:57:42 -0800

    Fixed syntax error

--- server/test/integration/rest.test.js
+++ server/test/integration/rest.test.js
@@ -3,7 +3,7 @@
  * Module dependencies
  */
 
-var request = require('request'),
+var request = require('request');
 var assert = require('assert');
 
 LOCAL_SERVER_URL = 'http://localhost:3000/'
@@ -13,7 +13,7 @@ describe('Run through an end-to-end test of the full REST API', function(){
     request.post(LOCAL_SERVER_URL + 'api/table', {form:{}}, function(err,resp,body){
       assert.equal(resp.statusCode, 200);
       console.log(resp);
-      done(); 
-    }); 
+      done();
+    });
   });
 });
