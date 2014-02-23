9ae801793fc6cd193dc2dc3a979b5c4843594797
Joseph Huang <yuzisee@gmail.com>
2014-02-22 20:09:42 -0800

    First unit test passing

--- server/test/integration/rest.test.js
+++ server/test/integration/rest.test.js
@@ -12,7 +12,11 @@ describe('Run through an end-to-end test of the full REST API', function(){
   it("should create a new table", function(done){
     request.post(LOCAL_SERVER_URL + 'api/table', {form:{}}, function(err,resp,body){
       assert.equal(resp.statusCode, 200);
-      console.log(resp);
+      var responseBody = JSON.parse(body);
+      assert.equal(typeof responseBody['id'], 'string');
+      assert.ok(responseBody['id'].length > 0); // We get an ID
+      assert.equal(responseBody['players'].length, 0); // No seated players yet at an empty table
+      assert.ok(responseBody['totalSeats'] > 2); // Some valid number of seats
       done();
     });
   });
